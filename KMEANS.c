4462f3864fd1c8c488b329306263f1e57d643772
ironmask2003 <lachithaperera75@gmail.com>
2025-03-02 00:54:06 +0100

    Try

--- KMEANS.c
+++ KMEANS.c
@@ -172,7 +172,7 @@ float euclideanDistance(float *point, float *center, int samples)
 	float dist=0.0;
 	for(int i=0; i<samples; i++) 
 	{
-		dist += (point[i]-center[i])*(point[i]-center[i]);
+		dist = fmaf((point[i]-center[i]), (point[i]-center[i]), dist);
 	}
 	dist = sqrt(dist);
 	return(dist);
