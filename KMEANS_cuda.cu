c825e697c679630b1db266caba49161d8cc11aba
ironmask2003 <lachithaperera75@gmail.com>
2025-03-02 21:04:14 +0100

    Try

--- KMEANS_cuda.cu
+++ KMEANS_cuda.cu
@@ -190,7 +190,7 @@ __device__ float d_euclideanDistance(float *point, float *center, int samples)
 	float dist=0.0;
 	for(int i=0; i<samples; i++) 
 	{
-		dist += (point[i]-center[i]) * (point[i]-center[i]);
+		dist = famf((point[i]-center[i]), (point[i]-center[i]), dist);
 	}
   sqrt(dist);
   return(dist);
