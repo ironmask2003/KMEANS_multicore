ce4da7395988cc0beddd48e4b08def45b29e3682
ironmask2003 <lachithaperera75@gmail.com>
2025-03-07 16:28:03 -0800

    Modified for Cluster testing

--- py_prog/run.py
+++ py_prog/run.py
@@ -1,4 +1,4 @@
-# Funzione che controlla riga per riga se due file sono uguali
+# Function used to check if two files have the same content
 def check_out(file1, file2):
     with open(file1, "r") as f1, open(file2, "r") as f2:
         for line1, line2 in zip(f1, f2):
@@ -6,28 +6,54 @@ def check_out(file1, file2):
                 return False
     return True
 
+# Function to set the job file of the MPI+OMP version
+def set_mpi_omp_job_file(vers, test, num_process, num_thread):
+    with open(f"./jobs/job_{vers}.sub", "w") as f:
+        f.write("universe = parallel\n\n")
+        f.write("executable = openmpiscript.sh\n\n")
+        f.write(f"arguments = ../KMEANS_mpi_omp input{test}.inp 40 100 1 0.0001 output_files/{vers}/output{test}.txt comp_time/{vers}/comp_time{test}.csv {num_thread}\n\n")
+        f.write("should_transfer_files = YES\n\n")
+        f.write(f"transfer_input_files = ../KMEANS_{vers} ../test_files/input{test}.inp\n\n")
+        f.write(f"output = logs_{vers}/out.$(NODE)\n\n")
+        f.write(f"error = logs_{vers}/err.$(NODE)\n\n")
+        f.write(f"log = logs_{vers}/log\n\n")
+        f.write(f"machine_count = {num_process}\n\n")
+        f.write(f"request_cpus = 32\n\n")
+        f.write("getenv = true\n\n")
+        f.write("queue\n")
 
-# Funzione che legge un file e prende il numero all'interno del file
-def take_num(file) -> float:
-    with open(file, "r") as f:
-        get_num = f.readline()
-        return float(get_num)
+# Function used to set the job file of the CUDA version
+def set_cuda_job_file(vers, test):
+    with open(f"./jobs/job_{vers}.sub", "w") as f:
+        f.write("universe = vanilla\n\n")
+        f.write("executable = ../KMEANS_cuda\n\n")
+        f.write(f"arguments = ../test_files/input{test}.inp 40 100 1 0.0001 output_files/{vers}/output{test}.txt comp_time/{vers}/comp_time{test}.csv\n\n")
+        f.write(f"log = logs_{vers}/{vers}_job.log\n\n")
+        f.write(f"output = logs_{vers}/{vers}_job.out\n\n")
+        f.write(f"error = logs_{vers}/{vers}_job.err\n\n")
+        f.write("request_gpus = 1\n\n")
+        f.write("getenv = true\n\n")
+        f.write("queue\n")
 
-
-# Prende in input un file .sh e lo esegue con un input per esempio, deve eseguire ./ciao.sh 100D
-def exec_file(file, test):
+# After setting the job file, commit the job and wait the execution of the job
+def exec_job(job_file, vers):
     import subprocess
 
-    # Voglio che il subprocess non stampi nulla a schermo
-    subprocess.run(["sh", file, test])
+    subprocess.run([f"condor_submit {job_file}"])
+    subprocess.run([f"condor_wait ./jobs/log_{vers}.log"])
     return
 
+# Function to run the test
+def run_test(vers, test, num_process, num_thread):
 
-def run_test(vers, test):
-    # Esecuzione della versione scelta per 100 volte
-    sh_file = f"bash_file/{vers}_run.sh"
-    exec_file(sh_file, test)
+    if vers == "omp_mpi": set_mpi_omp_job_file(vers, test, num_process, num_thread)
+    else: set_cuda_job_file(vers, test)
 
+    # Run the bash file
+    job_file = f"./jobs/job_{vers}.sub"
+    exec_job(job_file, test)
+
+    # Check if the output files between sequential and MPI+OMP are the same
     return check_out(
         f"output_files/seq/output{test}.txt",
         f"output_files/{vers}/output{test}.txt",
--- py_prog/tester.py
+++ py_prog/tester.py
@@ -6,23 +6,28 @@ from run import run_test
 
 
 # Function used to add AvgTime to csv file
-def addAvgTime(test_type, dimension, avgTime):
+def addAvgTime(test_type, dimension, avgTime, num_process, num_thread):
     # Check if the path not exist
     if os.path.exists(f"./{test_type}.csv") == False:
+
+        # Create the file and write the header
         open(f"./{test_type}.csv", "w").close()
         subprocess.run(
-            [f"echo Test dimension, AvgTime >> ./{test_type}.csv"], shell=True
+            [f"echo Test dimension, Number of Process, Number of thread, AvgTime >> ./{test_type}.csv"], shell=True
         )
 
-    subprocess.run([f"echo {dimension}, {avgTime} >> ./{test_type}.csv"], shell=True)
+    subprocess.run([f"echo {dimension}, {num_process}, {num_thread}, {avgTime} >> ./{test_type}.csv"], shell=True)
 
 
 class MyTest(unittest.TestCase):
-    def doTest(self, test_type, dimension):
-        self.assertEqual(run_test(test_type, dimension), True)
 
-    def test(self):
-        test_type = input("Enter test type: ")
+    # Function to run the test
+    def doTest(self, test_type, dimension, num_process, num_thread):
+        self.assertEqual(run_test(test_type, dimension, num_process, num_thread), True)
+
+    # Function to test the CUDA version
+    def test_cuda(self):
+        test_type = "cuda"
         dimensions = ["2D2", "10D", "20D", "2D", "100D", "100D2"]
 
         for dimension in dimensions:
@@ -47,7 +52,43 @@ class MyTest(unittest.TestCase):
 
             # AvgTime
             avgTime = sum(times) / len(times)
-            addAvgTime(test_type, dimension, avgTime)
+            addAvgTime(test_type, dimension, avgTime, 0, 0)
+
+    # Main function to run the test
+    def test(self):
+
+        # Check if the test is with cuda
+        test_type = input("Enter test type: ")
+        if test_type == "cuda": return self.test_cuda()
+
+        # Else test with OMP+MPI
+        dimensions = ["2D2", "10D", "20D", "2D", "100D", "100D2"]
+        num_process = [1, 2, 4, 8, 16, 32]
+        num_threads = [1, 2, 4, 8, 16, 32]
+
+        for proc, thread, dimension in [(p, t, d) for p in num_process for t in num_threads for d in dimensions]:
+            # Remove comp_time files if exist
+            if os.path.exists(f"comp_time/{test_type}/comp_time{dimension}.csv"):
+                os.remove(f"comp_time/{test_type}/comp_time{dimension}.csv")
+
+            # Create comp_time files
+            open(f"comp_time/{test_type}/comp_time{dimension}.csv", "w").close()
+
+            for _ in range(50):
+                self.doTest(test_type, dimension, proc, thread)
+
+            times = []
+
+            # Open comp_time file and read all lines
+            with open(f"comp_time/{test_type}/comp_time{dimension}.csv", "r") as f:
+                times = f.readlines()
+
+            # Convert all element in the array to float
+            times = [float(time) for time in times]
+
+            # AvgTime
+            avgTime = sum(times) / len(times)
+            addAvgTime(test_type, dimension, avgTime, proc, thread)
 
 
 if __name__ == "__main__":
