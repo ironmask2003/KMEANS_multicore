9ac2d01dd92dd335849362cb71ac75674f5e3c39
Ironmask2003 <lachithaperera75@gmail.com>
2025-03-10 00:46:33 +0000

    Version for testing on Cluster

--- py_prog/run.py
+++ py_prog/run.py
@@ -13,61 +13,73 @@ def check_out(file1, file2):
 # Function to set the job file of the MPI+OMP version
 def set_mpi_omp_job_file(vers, test, num_process, num_thread):
     # Check if logs directory exists
-    if not os.path.exists(f"./jobs/logs_{vers}/"):
-        os.mkdir(f"./jobs/logs_{vers}/")
+    if not os.path.exists(f"jobs/logs_{vers}/"):
+        os.mkdir(f"jobs/logs_{vers}/")
 
-    with open(f"./jobs/job_{vers}.sub", "w") as f:
+    with open(f"jobs/job_{vers}.sub", "w") as f:
         f.write("universe = parallel\n\n")
-        f.write("executable = openmpiscript.sh\n\n")
+        f.write("executable = jobs/openmpiscript.sh\n\n")
         f.write(
-            f"arguments = ./KMEANS_mpi_omp input{test}.inp 40 100 1 0.0001 output_files/{vers}/output{test}.txt comp_time/{vers}/comp_time{test}.csv {num_thread}\n\n"
+            f"arguments = ./KMEANS_{vers} input{test}.inp 40 100 1 0.0001 output{test}.txt comp_time{test}.csv {num_thread}\n\n"
         )
         f.write("should_transfer_files = YES\n\n")
         f.write(
-            f"transfer_input_files = ../KMEANS_{vers}, ../test_files/input{test}.inp\n\n"
+            f"transfer_input_files = KMEANS_{vers}, test_files/input{test}.inp \n\n"
         )
-        f.write(f"output = logs_{vers}/out.$(NODE)\n\n")
-        f.write(f"error = logs_{vers}/err.$(NODE)\n\n")
-        f.write(f"log = logs_{vers}/log\n\n")
+        f.write(f"output = jobs/logs_{vers}/out.$(NODE)\n\n")
+        f.write(f"error = jobs/logs_{vers}/err.$(NODE)\n\n")
+        f.write(f"log = jobs/logs_{vers}/log\n\n")
         f.write(f"machine_count = {num_process}\n\n")
         f.write("request_cpus = 32\n\n")
-        f.write("getenv = true\n\n")
+        f.write("getenv = True\n\n")
         f.write("queue\n")
 
 
 # Function used to set the job file of the CUDA version
 def set_cuda_job_file(vers, test):
     # Check if logs directory exists
-    if not os.path.exists(f"./jobs/logs_{vers}/"):
-        os.mkdir(f"./jobs/logs_{vers}/")
+    if not os.path.exists(f"jobs/logs_{vers}/"):
+        os.mkdir(f"jobs/logs_{vers}/")
+
+    with open(f"jobs/job_{vers}.sub", "w") as f:
+        f.write("universe = vanilla\n\n")
+        f.write("executable = ./KMEANS_cuda\n\n")
+        f.write(
+            f"arguments = test_files/input{test}.inp 40 100 1 0.0001 output_files/{vers}/output{test}.txt comp_time/{vers}/comp_time{test}.csv\n\n"
+        )
+        f.write(f"log = jobs/logs_{vers}/{vers}_job.log\n\n")
+        f.write(f"output = jobs/logs_{vers}/{vers}_job.out\n\n")
+        f.write(f"error = jobs/logs_{vers}/{vers}_job.err\n\n")
+        f.write("request_gpus = 1\n\n")
+        f.write("getenv = True\n\n")
+        f.write("queue\n")
+
+# Set job file for sequential test
+def set_seq_job_file(vers, test):
+    if not os.path.exists(f"jobs/logs_{vers}/"):
+        os.mkdir(f"jobs/logs_{vers}/")
 
     with open(f"./jobs/job_{vers}.sub", "w") as f:
         f.write("universe = vanilla\n\n")
-        f.write("executable = ../KMEANS_cuda\n\n")
-        f.write(
-            f"arguments = ../test_files/input{test}.inp 40 100 1 0.0001 output_files/{vers}/output{test}.txt comp_time/{vers}/comp_time{test}.csv\n\n"
-        )
-        f.write(f"log = logs_{vers}/{vers}_job.log\n\n")
-        f.write(f"output = logs_{vers}/{vers}_job.out\n\n")
-        f.write(f"error = logs_{vers}/{vers}_job.err\n\n")
-        f.write("request_gpus = 1\n\n")
-        f.write("getenv = true\n\n")
+        f.write("executable = ./KMEANS_seq\n\n")
+        f.write(f"arguments = test_files/input{test}.inp 40 100 1 0.0001 output_files/{vers}/output{test}.txt comp_time/{vers}/comp_time{test}.csv\n\n")
+        f.write(f"log = jobs/logs_{vers}/{vers}_job.log\n\n")
+        f.write(f"output = jobs/logs_{vers}/{vers}_job.out\n\n")
+        f.write(f"error = jobs/logs_{vers}/{vers}_job.err\n\n")
+        f.write("request_cpus = 1\n\n")
+        f.write("getenv = True\n\n")
         f.write("queue\n")
-
 
 # After setting the job file, commit the job and wait the execution of the job
 def exec_job(job_file, vers):
     import subprocess
 
-    subprocess.run([f"condor_submit {job_file}"])
-    subprocess.run([f"condor_wait ./jobs/log_{vers}.log"])
-    return
-
-
-def run_sh(bash_file, test):
-    import subprocess
-
-    subprocess.run(["sh", bash_file, test])
+    subprocess.run(["condor_submit", f"{job_file}"])
+    if vers == "omp_mpi":
+        subprocess.run(["condor_wait", f"jobs/logs_{vers}/log"])
+        return
+    else:
+        subprocess.run(["condor_wait", f"jobs/logs_{vers}/{vers}_job.log"])
     return
 
 
@@ -75,15 +87,20 @@ def run_sh(bash_file, test):
 def run_test(vers, test, num_process, num_thread):
     if vers == "omp_mpi":
         set_mpi_omp_job_file(vers, test, num_process, num_thread)
-        job_file = f"./jobs/job_{vers}.sub"
-        exec_job(job_file, test)
+        job_file = f"jobs/job_{vers}.sub"
+        exec_job(job_file, vers)
+        return check_out(
+            f"output{test}.txt",
+            f"output_files/seq/output{test}.txt"
+        )
     elif vers == "cuda":
         set_cuda_job_file(vers, test)
-        job_file = f"./jobs/job_{vers}.sub"
-        exec_job(job_file, test)
+        job_file = f"jobs/job_{vers}.sub"
+        exec_job(job_file, vers)
     else:
-        bash_file = f"bash_file/{vers}_run.sh"
-        run_sh(bash_file, test)
+        set_seq_job_file(vers, test)
+        job_file = f"jobs/job_{vers}.sub"
+        exec_job(job_file, vers)
 
     # Check if the output files between sequential and MPI+OMP are the same
     return check_out(
--- py_prog/tester.py
+++ py_prog/tester.py
@@ -34,22 +34,30 @@ class MyTest(unittest.TestCase):
 
     def running_test(self, test_type, dimension, num_process, num_thread):
         # Remove comp_time files if exist
-        if os.path.exists(f"comp_time/{test_type}/comp_time{dimension}.csv"):
-            os.remove(f"comp_time/{test_type}/comp_time{dimension}.csv")
+        if os.path.exists(f"./comp_time/{test_type}/comp_time{dimension}.csv"):
+            os.remove(f"./comp_time/{test_type}/comp_time{dimension}.csv")
 
         # Create comp_time files
-        open(f"comp_time/{test_type}/comp_time{dimension}.csv", "w").close()
+        open(f"./comp_time/{test_type}/comp_time{dimension}.csv", "w").close()
 
         for _ in range(50):
             self.doTest(
                 test_type, dimension, num_process, num_thread
             )  # In sqeuential and cuda test, num_process and num_thread are 0
+            # Check if the test is with omp_mpi for move the comp_tine in the correct directory
+            if test_type == "omp_mpi":
+                # Move comp_time in the correct file
+                with open(f"./comp_time{dimension}.csv", "r") as f:
+                    comp_time = f.readline().strip()
+                
+                with open(f"./comp_time/{test_type}/comp_time{dimension}.csv", "a") as f:
+                    f.write(comp_time + "\n")
 
         # Array to store all times
         times = []
 
         # Open comp_time file and read all lines
-        with open(f"comp_time/{test_type}/comp_time{dimension}.csv", "r") as f:
+        with open(f"./comp_time/{test_type}/comp_time{dimension}.csv", "r") as f:
             times = f.readlines()
 
         # Convert all element in the array to float
@@ -57,7 +65,7 @@ class MyTest(unittest.TestCase):
 
         # AvgTime
         avgTime = sum(times) / len(times)
-        addAvgTime(test_type, dimension, avgTime, 0, 0)
+        addAvgTime(test_type, dimension, avgTime, num_process, num_thread)
 
     # Function to test the CUDA version
     def test_cuda(self):
@@ -78,13 +86,15 @@ class MyTest(unittest.TestCase):
     # Main function to run the test
     def test(self):
         test_type = input("Enter test type: ")
+        subprocess.run(["make", f"KMEANS_{test_type}"])
 
         # Check if the test is with cuda
         if test_type == "cuda":
             return self.test_cuda()
         # Check if the test is teh sequential
         elif test_type == "seq":
-            return self.test_sequential()
+            self.test_sequential()
+            test_type = "omp_mpi"
 
         # Else test with OMP+MPI
         dimensions = ["2D2", "10D", "20D", "2D", "100D", "100D2"]
