344bfaced1897069930689bc1cf6be59b168d1af
ironmask2003 <lachithaperera75@gmail.com>
2025-04-04 00:28:51 +0200

    Added MPI_OMP test

--- py_prog/tester.py
+++ py_prog/tester.py
@@ -6,54 +6,60 @@ from run import main
 
 
 # Function used to add AvgTime to csv file
-def addAvgTime(test_type, avgTimes, pcs, thread):
+def addAvgTime(test_type, avgTimes):
     # Check if the path not exist
     if not os.path.exists(f"./test_csv/slurm/{test_type}_slurm.csv"):
         os.makedirs("./test_csv/slurm", exist_ok=True)
         # Create the file and write the header
         open(f"./test_csv/slurm/{test_type}_slurm.csv", "w").close()
-        if test_type != "omp_mpi":
-            subprocess.run(
-                [
-                    f"echo AvgTime2D2, AvgTime10D, AvgTime20D, AvgTime2D, AvgTime100D, AvgTime100D2 >> ./test_csv/slurm/{test_type}_slurm.csv"
-                ],
-                shell=True,
-            )
-        else:
-            subprocess.run(
-                [
-                    f"echo Number of Process, Number of Thread, AvgTime2D2, AvgTime10D, AvgTime20D, AvgTime2D, AvgTime100D, AvgTime100D2 >> ./test_csv/slurm/{test_type}_slurm.csv"
-                ],
-                shell=True,
-            )
+        subprocess.run(
+            [
+                f"echo AvgTime2D2, AvgTime10D, AvgTime20D, AvgTime2D, AvgTime100D, AvgTime100D2 >> ./test_csv/slurm/{test_type}_slurm.csv"
+            ],
+            shell=True,
+        )
 
-    if test_type != "omp_mpi":
+    subprocess.run(
+        [
+            f"echo {avgTimes[0]}, {avgTimes[1]}, {avgTimes[2]}, {avgTimes[3]}, {avgTimes[4]}, {avgTimes[5]} >> ./test_csv/slurm/{test_type}_slurm.csv"
+        ],
+        shell=True,
+    )
+
+
+def addAvgTime_omp_mpi(test_type, avgTimes, pcs, thread):
+    # Check if the path not exist
+    if not os.path.exists(f"./test_csv/slurm/{test_type}_{pcs}_slurm.csv"):
+        os.makedirs("./test_csv/slurm", exist_ok=True)
+        # Create the file and write the header
+        open(f"./test_csv/slurm/{test_type}_{pcs}_slurm.csv", "w").close()
         subprocess.run(
             [
-                f"echo {avgTimes[0]}, {avgTimes[1]}, {avgTimes[2]}, {avgTimes[3]}, {avgTimes[4]}, {avgTimes[5]} >> ./test_csv/slurm/{test_type}_slurm.csv"
+                f"echo Number of Process, Number of Thread, AvgTime2D2, AvgTime10D, AvgTime20D, AvgTime2D, AvgTime100D, AvgTime100D2 >> ./test_csv/slurm/{test_type}_{pcs}_slurm.csv"
             ],
             shell=True,
         )
-    else:
-        subprocess.run(
-            [
-                f"echo {pcs} {thread} {avgTimes[0]}, {avgTimes[1]}, {avgTimes[2]}, {avgTimes[3]}, {avgTimes[4]}, {avgTimes[5]} >> ./test_csv/slurm/{test_type}_slurm.csv"
-            ],
-            shell=True,
-        )
+
+    subprocess.run(
+        [
+            f"echo {pcs} {thread} {avgTimes[0]}, {avgTimes[1]}, {avgTimes[2]}, {avgTimes[3]}, {avgTimes[4]}, {avgTimes[5]} >> ./test_csv/slurm/{test_type}_{pcs}_slurm.csv"
+        ],
+        shell=True,
+    )
 
 
 class MyTest(unittest.TestCase):
     def doTest(self, vers: str, test: str, pcs: int, thread: int):
         self.assertEqual(main(vers, test, pcs, thread), True)
 
-    def main_test(self, dimensions: list[str], vers: str):
-        # Check the version of the program to run
-        if vers == "seq":
-            # Array of avgTime for each test
-            avgTimes = []
+    def run_omp_mpi(self, dimensions: list[str], vers: str):
+        processes = [2, 4, 8]
+        threads = [1, 2, 4, 8, 16, 32]
 
-            # iterate over all test file
+        for pcs, thread in [(p, t) for p in processes for t in threads]:
+            print(f"Test with {pcs} process and {thread} thread")
+            print("-----------------------------------------")
+            avgTimes = []
             for dim in dimensions:
                 # Remove comp_time files if exist
                 if os.path.exists(f"./comp_time/{vers}/comp_time{dim}.csv"):
@@ -65,7 +71,7 @@ class MyTest(unittest.TestCase):
                 print(f"Start testing with {dim}")
                 print("-----------------------------------------")
                 for _ in range(25):
-                    self.doTest(vers, dim, 0, 0)
+                    self.doTest(vers, dim, pcs, thread)
 
                 times = []
 
@@ -80,7 +86,46 @@ class MyTest(unittest.TestCase):
                 avgTime = sum(times) / len(times)
                 avgTimes.append(avgTime)
 
-            addAvgTime(vers, avgTimes, 0, 0)
+            addAvgTime_omp_mpi(vers, avgTimes, pcs, thread)
+            print("-----------------------------------------")
+
+    def main_test(self, dimensions: list[str], vers: str):
+        # Check the version of the program to run
+        if vers == "omp_mpi":
+            self.run_omp_mpi(dimensions, vers)
+            return
+        # Array of avgTime for each test
+        avgTimes = []
+
+        # iterate over all test file
+        for dim in dimensions:
+            # Remove comp_time files if exist
+            if os.path.exists(f"./comp_time/{vers}/comp_time{dim}.csv"):
+                os.remove(f"./comp_time/{vers}/comp_time{dim}.csv")
+
+            # Create comp_time files
+            open(f"./comp_time/{vers}/comp_time{dim}.csv", "w").close()
+
+            print(f"Start testing with {dim}")
+            print("-----------------------------------------")
+            for _ in range(25):
+                self.doTest(vers, dim, 0, 0)
+
+            times = []
+
+            # Open comp_time file and read all lines
+            with open(f"./comp_time/{vers}/comp_time{dim}.csv", "r") as f:
+                times = f.readlines()
+
+            # Convert all element in the array to float
+            times = [float(time) for time in times]
+
+            # AvgTime
+            avgTime = sum(times) / len(times)
+            avgTimes.append(avgTime)
+
+        addAvgTime(vers, avgTimes)
+        print("-----------------------------------------")
 
     def test(self):
         dimensions = ["2D2", "10D", "20D", "2D", "100D", "100D2"]
@@ -89,6 +134,16 @@ class MyTest(unittest.TestCase):
         print("-----------------------------------------")
         subprocess.run(["make", "KMEANS_seq"])
         self.main_test(dimensions, "seq")
+
+        print("Running CUDA test")
+        print("-----------------------------------------")
+        subprocess.run(["make", "KMEANS_cuda"])
+        self.main_test(dimensions, "cuda")
+
+        print("Running OMP_MPI test")
+        print("-----------------------------------------")
+        subprocess.run(["make", "KMEANS_omp_mpi"])
+        self.main_test(dimensions, "omp_mpi")
 
 
 if __name__ == "__main__":
